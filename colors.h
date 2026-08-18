689aef96e2123177879f7dbad01d27c333cc3649
rtvk2 <ritvik.su@gmail.com>
2026-08-18 17:05:42 +0530

    feat: add colors.h for formatting

---
+++ colors.h
@@ -0,0 +1,13 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define COLOR_RESET   "\033[0m"
+#define COLOR_USER    "\033[1;32m"   // green
+#define COLOR_HOST    "\033[1;32m"   // green
+#define COLOR_DIR     "\033[1;34m"   // blue
+#define COLOR_SYMBOLS "\033[0m"
+
+#define COLOR_ERROR   "\033[1;31m"   // red
+#define COLOR_WARN    "\033[1;33m"   // yellow
+
+#endif
